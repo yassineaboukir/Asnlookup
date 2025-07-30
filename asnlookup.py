e05c999415377405c4bacae179d6654a433cb047
Yassine ABOUKIR <9937267+yassineaboukir@users.noreply.github.com>
2025-07-30 11:28:58 +0800

    Update asnlookup.py

--- asnlookup.py
+++ asnlookup.py
@@ -1,17 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
+import os
 import sys
 import json
 import argparse
 import requests
-import os
 from termcolor import colored
-requests.packages.urllib3.disable_warnings()
 
-API = 'http://asnlookup.com/api/lookup?org='
-headers = {
-    'User-Agent': 'ASNLookup PY/Client'
-    }
+requests.packages.urllib3.disable_warnings()
 
 def banner():
         print('''
@@ -25,47 +21,54 @@ def parse_args():
     # parse the argument
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + ' -o twitter')
     org = parser.add_argument('-o', '--org', help='Organization to look up.', required=True)
+    apikey = parser.add_argument('-k', '--apikey', help='API key.', required=True)
     nmapscan = parser.add_argument('-n', '--nmapscan', help='Run Nmap port scanning.', required=False, action='store', nargs='?', const='-p 1-65535 -T4 -A -v')
     masscan = parser.add_argument('-m', '--masscan', help='Run Masscan port scanning.', required=False, action='store', nargs='?', const='-p0-65535 --rate 200')
     return parser.parse_args()
 
-def get_ip_space(organization):
+def get_ip_space(organization,apikey):
     # retrieve ip space from asnlookup.com
+    API_URL = 'https://asn-lookup.p.rapidapi.com/api?orgname='
+
+    headers = {
+        'User-Agent': 'ASNLookup PY/Client',
+        'X-Rapidapi-Key': apikey,
+    }
+
     try:
-        response = requests.get(API + organization.replace('_', ' '), headers = headers, timeout = 10)
+        response = requests.get(API_URL + organization.replace('_', ' '), headers = headers, timeout = 10)
+        # print(response)
         ip_space = json.loads(response.text)
+        # print(ip_space)
     except:
         print(colored('[!] We couldn\'t connect to asnlookup.com API. Try again!', 'red'))
         sys.exit(1)
 
-    if ip_space:
+    if ip_space and len(ip_space):
         path_ipv6 = os.path.dirname(os.path.realpath(__file__)) + '/output/' + organization + '_ipv6.txt'
         path_ipv4 = os.path.dirname(os.path.realpath(__file__)) + '/output/' + organization + '_ipv4.txt'
-        ipv4_exist =  False
-        ipv6_exist =  False
 
         if not os.path.exists('output'):
             os.makedirs('output')
         elif os.path.isfile('./output/' + organization + '.txt') == True:
             os.system('cd ./output/ && rm -f ' + organization + '.txt')
 
-        for ip in ip_space:
-            if ':' not in ip:
+        for item in ip_space:
+            # print(item)
+            # print(item['ipv4_prefix'])
+            if item['ipv4_prefix'] and len(item['ipv4_prefix']):
+                # print("\n".join(item['ipv4_prefix']))
+                ipv4_str = "\n".join(item['ipv4_prefix'])
+                print(ipv4_str)
                 with open('./output/' + organization + '_ipv4.txt', 'a') as dump:
-                    dump.write(ip + '\n')
-                    print(colored(ip, 'yellow'))
-                    ipv4_exist =  True
-            else:
+                    dump.write(ipv4_str+"\n")
+
+            if item['ipv6_prefix'] and len(item['ipv6_prefix']):
+                # print("\n".join(item['ipv6_prefix']))
+                ipv6_str = "\n".join(item['ipv6_prefix'])
+                print(ipv6_str)
                 with open('./output/' + organization + '_ipv6.txt', 'a') as dump:
-                    dump.write(ip + '\n')
-                    print(colored(ip, 'yellow'))
-                    ipv6_exist =  True
-
-        if ipv4_exist is True:
-            print(colored('\nIPv4: {}'.format(path_ipv4), 'green'))
-        if ipv6_exist is True:
-            print(colored('\nIPv6: {}'.format(path_ipv6), 'green'))
-
+                    dump.write(ipv6_str+"\n")
     else:
         print(colored('[!] Sorry! We couldn\'t find the organization\'s ASN.', 'red'))
 
@@ -93,10 +96,11 @@ def scanning(n, m, organization):
     else: pass
 
 if __name__ == '__main__':
-    banner()
-    nmapscan = parse_args().nmapscan
-    masscan = parse_args().masscan
-    org = parse_args().org \
-                            .replace(' ', '_')
-    get_ip_space(org)
+    # banner()
+    parser = parse_args()
+    nmapscan = parser.nmapscan
+    masscan = parser.masscan
+    org = parser.org.replace(' ', '_')
+    apikey = parser.apikey
+    get_ip_space(org,apikey)
     scanning(nmapscan, masscan, org)
